d96aea64eb8f67bb81825d6521c441ac39602649
Paopao <thianrawit.phan@gmail.com>
2026-03-02 09:54:14 +0700

    Add: Level Selector

---
+++ src/main/java/model/map/LevelSelectorMap.java
@@ -0,0 +1,135 @@
+package model.map;
+
+import model.entity.Entity;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class LevelSelectorMap {
+    private final int width;
+    private final int height;
+    private final Map<Point, java.util.List<Entity>> grid;
+    private final Map<Entity, Point> entityPositions;
+
+    public LevelSelectorMap(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.grid = new HashMap<>();
+        this.entityPositions = new HashMap<>();
+    }
+
+//    public LevelSelectorMap(LevelMap other) {
+//        this.width = other.width;
+//        this.height = other.height;
+//        this.grid = new HashMap<>();
+//        this.entityPositions = new HashMap<>();
+//        for (Entity entity : other.getEntities()) {
+//            Entity clonedEntity = new Entity(entity);
+//            Point position = other.entityPositions.get(entity);
+//            this.setPosition(clonedEntity, position.x, position.y);
+//        }
+//    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean isInside(int x, int y) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+
+    /**
+     * Add and set the position of an entity on the map.
+     * If the entity already exists, it will be moved to the new position.
+     *
+     * @param entity The entity to place or move.
+     * @param newX   The new x-coordinate.
+     * @param newY   The new y-coordinate.
+     */
+    public void setPosition(Entity entity, int newX, int newY) {
+        Point oldPosition = entityPositions.get(entity);
+        if (oldPosition != null) {
+            java.util.List<Entity> cell = grid.get(oldPosition);
+            if (cell != null) {
+                cell.remove(entity);
+                if (cell.isEmpty()) {
+                    grid.remove(oldPosition);
+                }
+            }
+        }
+
+        Point newPosition = new Point(newX, newY);
+        grid.computeIfAbsent(newPosition, k -> new ArrayList<>()).add(entity);
+        entityPositions.put(entity, newPosition);
+    }
+
+    /**
+     * Get the position of an entity on the map.
+     *
+     * @param entity The entity whose position is to be retrieved.
+     * @return A Point representing the entity's position.
+     * @throws IllegalStateException if the entity is not found on the map.
+     */
+    public Point getPosition(Entity entity) {
+        Point position = entityPositions.get(entity);
+        if (position == null) {
+            throw new IllegalStateException("Entity not found in map: " + entity.getEntityId());
+        }
+        return new Point(position);
+    }
+
+    /** Get the X coordinate of an entity on the map.
+     *
+     * @param entity The entity whose X coordinate is to be retrieved.
+     * @return The X coordinate of the entity.
+     * @throws IllegalStateException if the entity is not found on the map.
+     */
+    public int getX(Entity entity) {
+        return getPosition(entity).x;
+    }
+
+    /** Get the Y coordinate of an entity on the map.
+     *
+     * @param entity The entity whose Y coordinate is to be retrieved.
+     * @return The Y coordinate of the entity.
+     * @throws IllegalStateException if the entity is not found on the map.
+     */
+    public int getY(Entity entity) {
+        return getPosition(entity).y;
+    }
+
+    /** Get all entities at a specific position on the map.
+     *
+     * @param x The x-coordinate.
+     * @param y The y-coordinate.
+     * @return A list of entities at the specified position.
+     */
+    public java.util.List<Entity> getEntitiesAt(int x, int y) {
+        return grid.getOrDefault(new Point(x, y), java.util.List.of());
+    }
+
+    public java.util.List<Entity> getEntitiesAt(Point position) {
+        return getEntitiesAt(position.x, position.y);
+    }
+
+    /** Get all entites on the map.
+     *
+     * @return A list of all entities on the map.
+     */
+    public List<Entity> getEntities() {
+        return new ArrayList<>(entityPositions.keySet());
+    }
+
+    public Entity getEntityById(UUID entityId) {
+        return entityPositions.keySet().stream()
+                .filter(e -> e.getEntityId().equals(entityId))
+                .findFirst()
+                .orElse(null);
+    }
+}
--- src/main/java/state/MapState.java
+++ src/main/java/state/MapState.java
@@ -3,21 +3,76 @@ package state;
 import application.GameController;
 import application.Audio;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import logic.input.InputCommand;
 import logic.input.InputUtility;
+import model.entity.Entity;
+import model.entity.EntityType;
+import model.map.LevelSelectorLoader;
+import model.map.LevelSelectorMap;
+import utils.ImageUtils;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+import static application.Constant.*;
 
 public class MapState implements GameState {
-    /**
-     *
-     */
-    @Override
-    public void onEnter(GameStateEnum previousState) {
-        // Code here happens when entering the level selector (world map)
+
+    private LevelSelectorMap levelSelectorMap;
+
+    private final Map<Integer, String> FILE_NAME;
+
+    private static final Point[] SURROUNDING_DIRECTIONS = {
+            new Point(0, -1),
+            new Point(1, 0),
+            new Point(0, 1),
+            new Point(-1, 0)
+    };
+
+    private Point cursorPos;
+
+    public MapState(){
+        levelSelectorMap = LevelSelectorLoader.loadLevelSelector();
+        FILE_NAME = new HashMap<>();
+        FILE_NAME.put(1, "JAVA_IS_YOU");
+        FILE_NAME.put(2, "WHERE_DO_I_GO");
+        FILE_NAME.put(3, "NOW_WHAT_IS_THIS");
+        FILE_NAME.put(4, "OUT_OF_REACH");
+        FILE_NAME.put(5, "SUMMER_IN_THAILAND");
+        FILE_NAME.put(6, "ENCAPSULATED");
+        FILE_NAME.put(7, "CROSSING_RIVER");
+        FILE_NAME.put(8, "INHERITANCE");
+        FILE_NAME.put(9, "LOGIC_GATE");
+        FILE_NAME.put(10, "FETCH_DECODE_EXECUTE");
+        FILE_NAME.put(11, "STATIC_HAZARD");
+        FILE_NAME.put(12, "PROPAGATION_DELAY");
+        FILE_NAME.put(13, "SPEED_UP");
+        FILE_NAME.put(14, "TUNNEL_SPAGHETTI");
+        FILE_NAME.put(15, "CONDITIONALS");
+        FILE_NAME.put(16, "LOOP");
+        FILE_NAME.put(17, "NESTED_LOOP");
+        FILE_NAME.put(18, "DONT_TOUCH_IF_IT_WORKS");
+        FILE_NAME.put(19, "TRAVELLING_SALESMAN");
+        FILE_NAME.put(20, "LINKED_LIST");
+        FILE_NAME.put(21, "DURABILITY");
+        FILE_NAME.put(22, "HORIZONTAL_SCALING");
+        FILE_NAME.put(23, "TERNARY_RELATIONSHIP");
+        FILE_NAME.put(24, "REDUNDANCY");
+        FILE_NAME.put(25, "TRIGGER");
+        FILE_NAME.put(26, "STORED_PROCEDURE");
+        FILE_NAME.put(27, "JAR");
+        FILE_NAME.put(28, "GIT_REBASE");
+        FILE_NAME.put(29, "STACK_PANE");
+        FILE_NAME.put(30, "JAVA_IS_WIN");
+        cursorPos = new Point(1, 1);
     }
 
-    /**
-     *
-     */
+    @Override
+    public void onEnter(GameStateEnum previousState) {
+    }
+
     @Override
     public void onExit() {
         // Code here happens when exiting the level selector
@@ -28,6 +83,8 @@ public class MapState implements GameState {
      */
     @Override
     public void update() {
+        System.out.println(cursorPos.x + " " + cursorPos.y);
+
         InputCommand input = InputUtility.getTriggered();
         switch (input) {
             case MOVE_UP -> handleMoveUp();
@@ -40,31 +97,54 @@ public class MapState implements GameState {
 
     private void handleTrigger() {
         // TODO (MAP) : handle level selection with GameController.getInstance().playLevel(....); RETURN IF NOT POSSIBLE.
-        GameController.getInstance().playLevel("map/1_JAVA_IS_YOU.csv");
+        //GameController.getInstance().playLevel("map/" + 1 + "_" + FILE_NAME.get(1) + ".csv");
+        for(Entity e: levelSelectorMap.getEntitiesAt(cursorPos)){
+            for(int i=1; i<=30; i++){
+                if(e.getType().getTypeId().equals(Integer.toString(i))){
+                    GameController.getInstance().playLevel("map/" + i + "_" + FILE_NAME.get(i) + ".csv");
+                }
+            }
+        }
     }
 
     private void handleMoveRight() {
-        // TODO (MAP) : move cursor right if possible. RETURN IF NOT POSSIBLE.
-
-        Audio.playSfx("sound/SFX/select.wav");
+        if(cursorPos.x >= levelSelectorMap.getWidth() - 1) return;
+        for(Entity e: levelSelectorMap.getEntitiesAt(cursorPos.x+1, cursorPos.y)){
+            if(e.getType().getTypeId().equals("wire")){
+                cursorPos.x++;
+                Audio.playSfx("sound/SFX/select.wav");
+            }
+        }
     }
 
     private void handleMoveLeft() {
-        // TODO (MAP) : move cursor left if possible. RETURN IF NOT POSSIBLE.
-
-        Audio.playSfx("sound/SFX/select.wav");
+        if(cursorPos.x <= 0) return;
+        for(Entity e: levelSelectorMap.getEntitiesAt(cursorPos.x-1, cursorPos.y)){
+            if(e.getType().getTypeId().equals("wire")){
+                cursorPos.x--;
+                Audio.playSfx("sound/SFX/select.wav");
+            }
+        }
     }
 
     private void handleMoveDown() {
-        // TODO (MAP) : move cursor down if possible. RETURN IF NOT POSSIBLE.
-
-        Audio.playSfx("sound/SFX/select.wav");
+        if(cursorPos.y >= levelSelectorMap.getHeight() - 1) return;
+        for(Entity e: levelSelectorMap.getEntitiesAt(cursorPos.x, cursorPos.y+1)){
+            if(e.getType().getTypeId().equals("wire")){
+                cursorPos.y++;
+                Audio.playSfx("sound/SFX/select.wav");
+            }
+        }
     }
 
     private void handleMoveUp() {
-        // TODO (MAP) : move cursor up if possible. RETURN IF NOT POSSIBLE.
-
-        Audio.playSfx("sound/SFX/select.wav");
+        if(cursorPos.y <= 0) return;
+        for(Entity e: levelSelectorMap.getEntitiesAt(cursorPos.x, cursorPos.y-1)){
+            if(e.getType().getTypeId().equals("wire")){
+                cursorPos.y--;
+                Audio.playSfx("sound/SFX/select.wav");
+            }
+        }
     }
 
     /**
@@ -77,5 +157,68 @@ public class MapState implements GameState {
 
         gc.setFill(javafx.scene.paint.Color.rgb(0, 100, 100, 0.5));
         gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+
+        int xOffset = (int) (gc.getCanvas().getWidth() / 2) - (levelSelectorMap.getWidth() * SPRITE_SIZE) / 2;
+        int yOffset = (int) (gc.getCanvas().getHeight() / 2) - (levelSelectorMap.getHeight() * SPRITE_SIZE) / 2;
+        Point offset = new Point(xOffset, yOffset);
+
+        renderEntities(gc, offset);
+
     }
+
+    private void renderEntities(GraphicsContext gc, Point offset) {
+
+        long currentTime = System.currentTimeMillis();
+        int animationFrame = (int) ((currentTime / MILLISECONDS_PER_FRAME) % WOBBLE_FRAME_COUNT);
+
+        List<Entity> entities = levelSelectorMap.getEntities().stream()
+                .sorted(Comparator.comparingInt(e -> e.getType().getZIndex()))
+                .toList();
+
+        for (Entity entity : entities) {
+            EntityType entityType = entity.getType();
+            Image image = entityType.getSpriteSheet();
+
+            int gridX = levelSelectorMap.getX(entity);
+            int gridY = levelSelectorMap.getY(entity);
+            int drawX = SPRITE_SIZE * gridX + offset.x;
+            int drawY = SPRITE_SIZE * gridY + offset.y;
+
+            int spriteRow = switch (entityType.getAnimationStyle()) {
+                case WOBBLE -> 0;
+                case TILED -> getSurroundingNumber(entity, levelSelectorMap);
+                case DIRECTIONAL -> getDirectionalNumber(entity);
+            };
+
+            ImageUtils.drawSprite(gc, image, animationFrame, spriteRow, drawX, drawY);
+            gc.setEffect(null);
+        }
+        ImageUtils.drawSprite(gc, new Image("sprite/JAVA.png"), animationFrame, 0, SPRITE_SIZE * cursorPos.x + offset.x, SPRITE_SIZE * cursorPos.y + offset.y);
+    }
+
+    private int getSurroundingNumber(Entity entity, LevelSelectorMap levelMap) {
+        int surroundingNumber = 0;
+        for (int direction = 0; direction < 4; direction++) {
+            List<Entity> surroundingEntities = levelMap.getEntitiesAt(
+                    levelMap.getX(entity) + SURROUNDING_DIRECTIONS[direction].x,
+                    levelMap.getY(entity) + SURROUNDING_DIRECTIONS[direction].y
+            );
+            boolean hasSurroundingInDirection = surroundingEntities.stream()
+                    .anyMatch(e -> e.getType() == entity.getType());
+            if (hasSurroundingInDirection) {
+                surroundingNumber += (1 << direction);
+            }
+        }
+        return surroundingNumber;
+    }
+
+    private int getDirectionalNumber(Entity entity) {
+        return switch (entity.getDirection()) {
+            case UP -> 0;
+            case RIGHT -> 1;
+            case LEFT -> 2;
+            case DOWN -> 3;
+        };
+    }
+
 }
